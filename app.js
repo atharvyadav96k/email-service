0203d65580b472e255dddb86ff2d1d94afb6fe2b
Atharv Yadav <atharvyadav96k@gmail.com>
2026-01-06 21:31:29 +0530

    error fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import nodemailer from "nodemailer";
+const express = require("express");
+const nodemailer = require("nodemailer")
 
 const app = express();
 app.use(express.json());
